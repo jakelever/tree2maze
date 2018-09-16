8d6456a6fcb64a1ad36b80e05972df87bd525a03
Jake Lever <jlever@bcgsc.ca>
2018-09-15 21:40:18 -0700

    Almost working with polyline:

--- tree2maze.py
+++ tree2maze.py
@@ -100,20 +100,22 @@ if __name__ == '__main__':
 	#startLocs = [(0,-1)]
 
 	#getChains(1,[(0,-1),(0,1)])
-	segments = []
-	segments.append((0,0,0,-1,'root'))
+	segments = defaultdict(list)
+	segments['root'] = [(0,0)]
 
-	for layer in range(1,6):
+	for layer in range(1,5):
 		chains = getChains(layer,active)
 		
 		doSplit = True
 
 		for name,chain in chains.items():
 
-			for (x1,y1),(x2,y2) in zip(chain[:-1],chain[1:]):
+			#for (x1,y1),(x2,y2) in zip(chain[:-1],chain[1:]):
 				#dwg.add(dwg.line((x1,y1),(x2,y2)))
 				#dwg.add(dwg.line((x1,y1),(x2,y2), stroke=svgwrite.rgb(10, 10, 16, '%')))
-				segments.append((x1,y1,x2,y2,name))
+				#segments[name].append((x1,y1))
+			#	segments[name].append((x2,y2))
+			segments[name] += chain
 
 			# Assign the characters to the grid
 			#for x,y in chain:
@@ -145,7 +147,9 @@ if __name__ == '__main__':
 					lastX,lastY = subchain[-1]
 					newX,newY = nextGridPointOut(layer,lastX,lastY)
 					active[child] = (newX,newY)
-					segments.append((lastX,lastY,newX,newY,child))
+					#segments.append((lastX,lastY,newX,newY,child))
+					segments[child].append((lastX,lastY))
+					segments[child].append((newX,newY))
 					print("Starting %s at (%d,%d)" % (child,lastX,lastY))
 					chars[child] = child
 
@@ -153,7 +157,9 @@ if __name__ == '__main__':
 				lastX,lastY = chain[-1]
 				newX,newY = nextGridPointOut(layer,lastX,lastY)
 				active[name] = (newX,newY)
-				segments.append((lastX,lastY,newX,newY,name))
+				#segments.append((lastX,lastY,newX,newY,name))
+				segments[child].append((lastX,lastY))
+				segments[child].append((newX,newY))
 
 
 		print(chains)
@@ -161,30 +167,46 @@ if __name__ == '__main__':
 
 	#turtles = []
 	#turtles.append( (0,-1,RIGHT) )
+	for name,coords in segments.items():
+		print(name, coords)
 
-	minX = min ( min(x1,x2) for x1,y1,x2,y2,_ in segments )
-	minY = min ( min(y1,y2) for x1,y1,x2,y2,_ in segments )
-	maxY = max ( max(y1,y2) for x1,y1,x2,y2,_ in segments )
+	minX = min ( x for name,coords in segments.items() for x,y in coords )
+	maxX = max ( y for name,coords in segments.items() for x,y in coords )
+	maxY = max ( y for name,coords in segments.items() for x,y in coords )
 
 	rand_color = randomcolor.RandomColor()
 	colors = defaultdict( lambda : rand_color.generate() )
 
-	dwg.add(dwg.rect(insert=(0, 0), size=('100%', '100%'), rx=None, ry=None, fill='rgb(0,0,0)'))
-	for x1,y1,x2,y2,name in segments:
-		x1 = 10*(x1-minX)
-		x2 = 10*(x2-minX)
-		y1 = 10*(maxY-y1)
-		y2 = 10*(maxY-y2)
-		color = colors[name][0]
-		#dwg.add(dwg.line((x1,y1),(x2,y2), stroke='rgb(255,255,255)', stroke_width=5))
-		#dwg.add(dwg.line((x1,y1),(x2,y2), stroke=color, stroke_width=5))
-		g = svgwrite.container.Group()
-		g.add(dwg.line((x1,y1),(x2,y2), stroke=color, stroke_width=5))
-		g.set_desc(name)
-		dwg.add(g)
-		#dwg.add(dwg.polygon(points, fill='rgb(255,255,255)'))
+	print(minX, maxX, maxY)
 
-	#for layer:wq in range(1):
-	#	for (startX,startY,startDir) in turtles:
+	dwg.add(dwg.rect(insert=(0, 0), size=('100%', '100%'), rx=None, ry=None, fill='rgb(0,0,0)'))
+	if False:
+		for name,coords in segments.items():
+			for (x1,y1),(x2,y2) in zip(coords[:-1],coords[1:]):
+				x1 = 10*(x1-minX)
+				x2 = 10*(x2-minX)
+				y1 = 10*(maxY-y1)
+				y2 = 10*(maxY-y2)
+				#coords = [ (10*(x-minX),10*(maxY-y)) for x,y in coords ]
+
+				color = colors[name][0]
+				#dwg.add(dwg.line((x1,y1),(x2,y2), stroke='rgb(255,255,255)', stroke_width=5))
+				#dwg.add(dwg.line((x1,y1),(x2,y2), stroke=color, stroke_width=5))
+				g = svgwrite.container.Group()
+				#g.add(dwg.polyline(coords, stroke=color, stroke_width=5))
+				g.add(dwg.line((x1,y1),(x2,y2), stroke=color, stroke_width=5))
+				g.set_desc(name)
+				dwg.add(g)
+				#dwg.add(dwg.polygon(points, fill='rgb(255,255,255)'))
+	else:	
+		for name,coords in segments.items():
+			coords = [ (10*(x-minX),10*(maxY-y)) for x,y in coords ]
+
+			color = colors[name][0]
+			g = svgwrite.container.Group()
+			g.add(dwg.polyline(coords, stroke=color, stroke_width=5, fill='none'))
+			g.set_desc(name)
+			dwg.add(g)
+
 	dwg.save()
 
